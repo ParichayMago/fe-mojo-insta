31853600df8608a80571486d906a9ba21aadffcc
ParichayMago <parichaymago@gmail.com>
2025-03-01 22:26:51 +0530

    redire url

--- src/Pages/Auth.tsx
+++ src/Pages/Auth.tsx
@@ -19,7 +19,7 @@ const Auth = () => {
 
   const handleLogin = () => {
     location.href =
-      "https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1169547817377137&redirect_uri=https://fe-mojo-insta.vercel.app/auth/token/&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights";
+      `${import.meta.env.VITE_reurl}`;
   };
 
   return (
